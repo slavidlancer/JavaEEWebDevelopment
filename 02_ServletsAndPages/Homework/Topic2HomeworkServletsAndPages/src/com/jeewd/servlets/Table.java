cf80f81794137e4578dcae974b7f3ddb885df39e
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-24 00:45:51 +0200

    refactor Table and Form servlets

--- 02_ServletsAndPages/Homework/Topic2HomeworkServletsAndPages/src/com/jeewd/servlets/Table.java
+++ 02_ServletsAndPages/Homework/Topic2HomeworkServletsAndPages/src/com/jeewd/servlets/Table.java
@@ -3,7 +3,6 @@ package com.jeewd.servlets;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
