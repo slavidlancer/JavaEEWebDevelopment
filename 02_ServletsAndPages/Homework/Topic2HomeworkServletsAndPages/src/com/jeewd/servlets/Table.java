ae1dfe36aa2498d7cb687a31278f30ae9fea35e0
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-23 23:56:15 +0200

    modify Table servlet to show correct Date object, refactor html code

--- 02_ServletsAndPages/Homework/Topic2HomeworkServletsAndPages/src/com/jeewd/servlets/Table.java
+++ 02_ServletsAndPages/Homework/Topic2HomeworkServletsAndPages/src/com/jeewd/servlets/Table.java
@@ -1,6 +1,7 @@
 package com.jeewd.servlets;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -40,19 +41,41 @@ public class Table extends HttpServlet {
     }
 
     private String formatTable() {
+        Date firstDate = getDateAndTimeAsObject("21.01.2016 18:00");
+        Date secondDate = getDateAndTimeAsObject("28.01.2016 18:00");
+        
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("<!DOCTYPE html>");
         stringBuilder.append("<html>");
         stringBuilder.append("<head><title>").append("Table Servlet").
             append("</title></head>");
         stringBuilder.append("<body>");
-        stringBuilder.append("<table border=\"1\" class=\"width: 100%\">" +
-                "<tbody><tr><th>Topic</th><th>Date</th></tr><tr>" +
-                "<td>Java Web Development</td>" + "<td>" + new Date() +
-                "</td></tr></tbody></table>");
+        stringBuilder.append("<table border=\"1\" class=\"width: 100%\">");
+        stringBuilder.append("<tbody><tr><th>Topic</th><th>Date</th></tr>");
+        stringBuilder.append("<tr><td>Web Basics</td>");
+        stringBuilder.append("<td>" + firstDate + "</td>");
+        stringBuilder.append("</tr>");
+        stringBuilder.append("<tr><td>Servlets and Pages</td>");
+        stringBuilder.append("<td>" + secondDate + "</td>");
+        stringBuilder.append("</tr>");
+        stringBuilder.append("</tbody></table>");
         stringBuilder.append("</body>");
         stringBuilder.append("</html>");
 
         return stringBuilder.toString();
+    }
+    
+    private Date getDateAndTimeAsObject(String date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+                "dd.MM.yyyy HH:mm");
+        Date dateObject = null;
+        
+        try {
+            dateObject = simpleDateFormat.parse(date);
+        } catch (java.text.ParseException pe) {
+            pe.printStackTrace();
+        }
+        
+        return dateObject;
     }
 }
