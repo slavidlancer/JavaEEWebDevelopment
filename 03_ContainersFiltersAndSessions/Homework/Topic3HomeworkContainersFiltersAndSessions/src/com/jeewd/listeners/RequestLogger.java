94e70066c0eb49d7f88600bd7e9ee61104c82e65
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-30 00:14:51 +0200

    improve the request logger (listener)

--- 03_ContainersFiltersAndSessions/Homework/Topic3HomeworkContainersFiltersAndSessions/src/com/jeewd/listeners/RequestLogger.java
+++ 03_ContainersFiltersAndSessions/Homework/Topic3HomeworkContainersFiltersAndSessions/src/com/jeewd/listeners/RequestLogger.java
@@ -1,5 +1,26 @@
 package com.jeewd.listeners;
 
-public class RequestLogger {
-    
+import java.util.Date;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServletRequest;
+
+@WebListener
+public class RequestLogger implements ServletRequestListener {
+    @Override
+    public void requestDestroyed(ServletRequestEvent requestEvent) {}
+
+    @Override
+    public void requestInitialized(ServletRequestEvent requestEvent) {
+        Date dateValue = new Date();
+        HttpServletRequest servletRequest = (HttpServletRequest)
+                requestEvent.getServletRequest();
+        System.out.println("\tRequest");
+        System.out.println("IP address: " + servletRequest.getRemoteAddr());
+        System.out.println("Session ID: " + servletRequest.getSession().
+                getId());
+        System.out.println("Type: " + servletRequest.getDispatcherType());
+        System.out.println("Date and time: " + dateValue);
+    }
 }
