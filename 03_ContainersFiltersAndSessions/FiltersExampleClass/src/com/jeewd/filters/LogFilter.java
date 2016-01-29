8a7bbf65c6a83db57b1e09a2dd6873e9a87c712c
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-30 00:18:57 +0200

    add log filter

---
+++ 03_ContainersFiltersAndSessions/FiltersExampleClass/src/com/jeewd/filters/LogFilter.java
@@ -0,0 +1,28 @@
+package com.jeewd.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class LogFilter implements Filter {
+    @Override
+    public void destroy() {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        System.out.println("Filter: " + request.getLocalAddr());
+        
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {}
+
+}
