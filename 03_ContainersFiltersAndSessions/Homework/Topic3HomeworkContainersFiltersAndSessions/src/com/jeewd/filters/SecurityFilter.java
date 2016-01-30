f022cf8cece33c5c6cb6f4e645bc400a833e9ff4
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-31 00:38:38 +0200

    add support for username and password, check for existence and proper values, process login requests and redirect properly

--- 03_ContainersFiltersAndSessions/Homework/Topic3HomeworkContainersFiltersAndSessions/src/com/jeewd/filters/SecurityFilter.java
+++ 03_ContainersFiltersAndSessions/Homework/Topic3HomeworkContainersFiltersAndSessions/src/com/jeewd/filters/SecurityFilter.java
@@ -8,11 +8,16 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebFilter("/pages/Homepage.jsp")
 public class SecurityFilter implements Filter {
+    private final String USER_NAME = "nickname";
+    private final String PASS_WORD = "0123456789";
+    
     @Override
     public void destroy() {}
 
@@ -20,15 +25,24 @@ public class SecurityFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String userName = (String) httpRequest.getSession().
-                getAttribute("username");
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession httpSession = httpRequest.getSession();
+        String userName = (String) httpSession.getAttribute("username");
         String requestUserName = request.getParameter("user");
+        String requestPassWord = request.getParameter("pwd");
         
-        if ((userName == null) && (requestUserName == null)) {
-            ((HttpServletResponse) response).sendRedirect(
-                    "Loginform.jsp");
-        } else if (requestUserName != null) {
-            httpRequest.getSession().setAttribute("username", requestUserName);
+        if ((requestUserName != null) && (requestPassWord != null)) {
+            if (requestUserName.equals(USER_NAME) &&
+                    requestPassWord.equals(PASS_WORD)) {
+                httpSession.setAttribute("username", requestUserName);
+                httpSession.setMaxInactiveInterval(30 * 60);
+                Cookie userNameCookie = new Cookie("username", requestUserName);
+                userNameCookie.setMaxAge(30 * 60);
+                httpResponse.addCookie(userNameCookie);
+                httpResponse.sendRedirect("Homepage.jsp");
+            } else if (userName == null) {
+                httpResponse.sendRedirect("Loginform.jsp");
+            }
         }
         
         chain.doFilter(request, response);
