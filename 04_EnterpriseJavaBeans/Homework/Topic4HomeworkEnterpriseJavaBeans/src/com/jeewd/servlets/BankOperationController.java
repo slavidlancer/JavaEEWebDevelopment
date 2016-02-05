94239bfbc3b4fa4e4b16af14e7414eab7fdf11c4
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-06 00:23:54 +0200

    add EJB, HTTP request/response/session, get parameters, set attribute, redirect to the JSP

--- 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/servlets/BankOperationController.java
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/servlets/BankOperationController.java
@@ -1,15 +1,22 @@
 package com.jeewd.servlets;
 
 import java.io.IOException;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.jeewd.ejbs.BankOperation;
 
 @WebServlet("/BankOperationController")
 public class BankOperationController extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    
+    @EJB(beanName = "BankOperationImpl")
+    private BankOperation bankOperation;
 
     public BankOperationController() {
         super();
@@ -17,12 +24,28 @@ public class BankOperationController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(
-                request.getContextPath());
+        //response.getWriter().append(request.getContextPath());
     }
 
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        //doGet(request, response);
+        response.getWriter().println("Processing Bank Operations .... "
+                + "Please wait");
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession httpSession = httpRequest.getSession();
+        
+        String client = request.getParameter("id");
+        double currentAmount = (Double.parseDouble(request.getParameter(
+                "currentamount"))); //try-catch
+        String operation = request.getParameter("operation");
+        double changeAmount = (Double.parseDouble(request.getParameter(
+                "changeamount"))); //try-catch
+        
+        httpSession.setAttribute("id", "test");
+        
+        httpResponse.sendRedirect("pages/Webbankingpage.jsp");
     }
 }
