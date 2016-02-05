cf7adbf89edbd0200d340447b562362b6d3b1112
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-05 22:18:56 +0200

    add stateless, stateful and singleton EJBs to the TestServlets

--- 04_EnterpriseJavaBeans/EjbsExampleClass/src/com/jeewd/servlets/TestServlet.java
+++ 04_EnterpriseJavaBeans/EjbsExampleClass/src/com/jeewd/servlets/TestServlet.java
@@ -1,15 +1,32 @@
 package com.jeewd.servlets;
 
 import java.io.IOException;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.jeewd.ejbs.DateCollection;
+import com.jeewd.ejbs.SingletonBean;
+import com.jeewd.ejbs.StatefulHelloWorld;
+import com.jeewd.ejbs.StatelessHelloWorld;
 
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    
+    @EJB(beanName = "StatelessHelloWorldImpl")
+    private StatelessHelloWorld statelessHelloWorld;
+    
+    @EJB
+    private StatefulHelloWorld statefulHelloWorld;
+    
+    @EJB
+    private SingletonBean singletonBean;
+    
+    @EJB
+    private DateCollection dateCollection;
 
     public TestServlet() {
         super();
@@ -17,8 +34,10 @@ public class TestServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(
-                request.getContextPath());
+        response.getWriter().println(statelessHelloWorld.helloStatelessEjb());
+        response.getWriter().println(statefulHelloWorld.statefulHelloWorld());
+        response.getWriter().println(singletonBean.singletonHelloWorld());
+        response.getWriter().println(dateCollection.addAndReturn());
     }
 
     protected void doPost(HttpServletRequest request,
--- 04_EnterpriseJavaBeans/EjbsExampleClass/src/com/jeewd/servlets/TestServlet2.java
+++ 04_EnterpriseJavaBeans/EjbsExampleClass/src/com/jeewd/servlets/TestServlet2.java
@@ -1,15 +1,28 @@
 package com.jeewd.servlets;
 
 import java.io.IOException;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.jeewd.ejbs.SingletonBean;
+import com.jeewd.ejbs.StatefulHelloWorld;
+import com.jeewd.ejbs.StatelessHelloWorld;
 
 @WebServlet("/TestServlet2")
 public class TestServlet2 extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    
+    @EJB(beanName = "StatelessHelloWorldImpl")
+    private StatelessHelloWorld statelessHelloWorld;
+    
+    @EJB
+    private StatefulHelloWorld statefulHelloWorld;
+    
+    @EJB
+    private SingletonBean singletonBean;
 
     public TestServlet2() {
         super();
@@ -17,8 +30,9 @@ public class TestServlet2 extends HttpServlet {
 
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(
-                request.getContextPath());
+        response.getWriter().println(statelessHelloWorld.helloStatelessEjb());
+        response.getWriter().println(statefulHelloWorld.statefulHelloWorld());
+        response.getWriter().println(singletonBean.singletonHelloWorld());
     }
 
     protected void doPost(HttpServletRequest request,
