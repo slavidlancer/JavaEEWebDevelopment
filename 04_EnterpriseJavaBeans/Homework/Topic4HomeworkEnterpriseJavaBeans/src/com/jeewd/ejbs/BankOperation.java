b202e659048ce4977e03b8b6c4d8e308cc7ae94d
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-06 09:38:59 +0200

    add EJB interface

--- 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/BankOperation.java
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/BankOperation.java
@@ -4,5 +4,7 @@ import javax.ejb.Local;
 
 @Local
 public interface BankOperation {
-    
+    double deposit(String client, double currentAmount, double changeAmount);
+    double withdraw(String client, double currentAmount, double changeAmount);
+    boolean doesNotContainClient(String client);
 }
