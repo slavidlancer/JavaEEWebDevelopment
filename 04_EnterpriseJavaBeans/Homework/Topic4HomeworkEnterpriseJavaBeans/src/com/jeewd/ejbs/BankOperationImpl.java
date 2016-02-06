bbfaf7d870a5eaa64ade3c7432ff9abbb6a46808
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-06 20:25:08 +0200

    modify the logic implementation, process correct/incorrect values and new/existing clients

--- 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/BankOperationImpl.java
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/BankOperationImpl.java
@@ -8,40 +8,66 @@ import javax.ejb.Stateless;
 public class BankOperationImpl implements BankOperation {
     private Map<String, Double> clientAccount = new HashMap<>();
     private boolean newClientAdded;
+    private boolean incorrectValues;
 
     @Override
     public double deposit(String client, double currentAmount,
             double changeAmount) {
-        clientAccount.put(client, (currentAmount + changeAmount));
+        if (clientAccount.containsKey(client)) {
+            currentAmount = clientAccount.get(client);
+            newClientAdded = false;
+        } else {
+            clientAccount.put(client, currentAmount);
+            newClientAdded = true;
+        }
         
-        return currentAmount + changeAmount;
+        if (changeAmount > 0.0d) {
+            clientAccount.put(client, (currentAmount + changeAmount));
+            incorrectValues = false;
+            
+            return currentAmount + changeAmount;
+        }
+        
+        incorrectValues = true;
+        
+        return currentAmount;
     }
 
     @Override
     public double withdraw(String client, double currentAmount,
             double changeAmount) {
-        if (doesNotContainClient(client)) {
+        if (!clientAccount.containsKey(client)) {
             newClientAdded = true;
             
             return deposit(client, currentAmount, changeAmount);
+        } else if (clientAccount.containsKey(client)) {
+            currentAmount = clientAccount.get(client);
+            newClientAdded = false;
         }
         
-        clientAccount.put(client, (currentAmount - changeAmount));
+        if ((changeAmount > 0.0d) && (changeAmount <= (0.50 * currentAmount))) {
+            clientAccount.put(client, (currentAmount - changeAmount));
+            incorrectValues = false;
+            
+            return currentAmount - changeAmount;
+        }
         
-        return currentAmount - changeAmount;
+        incorrectValues = true;
         
+        return currentAmount;
     }
 
     @Override
     public boolean doesNotContainClient(String client) {
         if (!clientAccount.containsKey(client) || newClientAdded) {
-            newClientAdded = false;
-            
             return true;
         }
         
-        newClientAdded = false;
-        
         return false;
+    }
+
+    @Override
+    public boolean incorrectAmmountToChange() {
+        return incorrectValues;
     }
 }
