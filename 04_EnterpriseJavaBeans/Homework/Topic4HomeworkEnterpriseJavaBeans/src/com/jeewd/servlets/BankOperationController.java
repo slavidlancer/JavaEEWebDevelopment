4f7aa31a3564a5cb7a5ac7aaf8d509ee5fa40249
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-06 09:41:15 +0200

    control the flow of operations, return proper messages and values

--- 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/servlets/BankOperationController.java
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/servlets/BankOperationController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import com.jeewd.ejbs.BankOperation;
 
 @WebServlet("/BankOperationController")
@@ -31,20 +30,40 @@ public class BankOperationController extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         //doGet(request, response);
         response.getWriter().println("Processing Bank Operations .... "
-                + "Please wait");
+                + "\nPlease wait ....\nPossible error occurred!\nPlease enter"
+                + "proper values!");
         
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         HttpSession httpSession = httpRequest.getSession();
+        httpSession.setAttribute("responsemessage", "");
         
         String client = request.getParameter("id");
         double currentAmount = (Double.parseDouble(request.getParameter(
-                "currentamount"))); //try-catch
+                "currentamount").replaceAll(",", ""))); //try-catch
         String operation = request.getParameter("operation");
         double changeAmount = (Double.parseDouble(request.getParameter(
-                "changeamount"))); //try-catch
+                "changeamount").replaceAll(",", ""))); //try-catch
         
-        httpSession.setAttribute("id", "test");
+        if (operation.equals("deposit")) {
+            currentAmount = bankOperation.deposit(client, currentAmount,
+                    changeAmount);
+        } else if (operation.equals("withdraw")) {
+            currentAmount = bankOperation.withdraw(client, currentAmount,
+                    changeAmount);
+        } else {
+            response.getWriter().println("Unknown operation!");
+        }
+        
+        if (bankOperation.doesNotContainClient(client)) {
+            httpSession.setAttribute("responsemessage", "New Client, no "
+                    + "withdraws are allowed! Deposits only.");
+        }
+        
+        System.out.println(client + " " + currentAmount + " " + changeAmount);
+        
+        httpSession.setAttribute("id", client);
+        httpSession.setAttribute("currentamount", currentAmount);
         
         httpResponse.sendRedirect("pages/Webbankingpage.jsp");
     }
