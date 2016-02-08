baf1df8d86565d71383c0641a3b7909613dfe391
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-08 23:11:29 +0200

    handle different currencies (change amount), exchange rates conversions, simple version

--- 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/servlets/BankOperationController.java
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/servlets/BankOperationController.java
@@ -15,6 +15,9 @@ import com.jeewd.ejbs.CurrencyConversion;
 @WebServlet("/BankOperationController")
 public class BankOperationController extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static final String currencyBGN = "bgn";
+    private static final String currencyUSD = "usd";
+    private static final String currencyEUR = "eur";
     
     @EJB(beanName = "BankOperationImpl")
     private BankOperation bankOperation;
@@ -50,6 +53,7 @@ public class BankOperationController extends HttpServlet {
         BigDecimal currentAmount = new BigDecimal(0);
         String accountCurrency = request.getParameter("accountcurrency");
         BigDecimal changeAmount = new BigDecimal(0);
+        String changeCurrency = request.getParameter("changecurrency");
         boolean incorrectBigDecimalValues = false;
         
         try {
@@ -72,6 +76,70 @@ public class BankOperationController extends HttpServlet {
             client = "undefined client";
         } else {
             client = rawClient;
+        }
+        
+        if (BankOperationController.currencyBGN.equals(accountCurrency)) {
+            bankOperation.setCurrency(client,
+                    BankOperationController.currencyBGN);
+            httpSession.setAttribute("selectedbgn", "selected");
+            httpSession.setAttribute("selectedusd", "");
+            httpSession.setAttribute("selectedeur", "");
+            
+            if (!accountCurrency.equals(changeCurrency)) {
+                if (BankOperationController.currencyUSD.
+                        equals(changeCurrency)) {
+                    changeAmount = currencyConversion.
+                            convertFromCurrencyToAnother(changeAmount,
+                                    new BigDecimal(1.76185));
+                } else if (BankOperationController.currencyEUR.
+                        equals(changeCurrency)) {
+                    changeAmount = currencyConversion.
+                            convertFromCurrencyToAnother(changeAmount,
+                                    new BigDecimal(1.95583)); //Euro is fixed
+                }
+            }
+        } else if (BankOperationController.currencyUSD.
+                equals(accountCurrency)) {
+            bankOperation.setCurrency(client,
+                    BankOperationController.currencyUSD);
+            httpSession.setAttribute("selectedbgn", "");
+            httpSession.setAttribute("selectedusd", "selected");
+            httpSession.setAttribute("selectedeur", "");
+            
+            if (!accountCurrency.equals(changeCurrency)) {
+                if (BankOperationController.currencyBGN.
+                        equals(changeCurrency)) {
+                    changeAmount = currencyConversion.
+                            convertFromCurrencyToAnother(changeAmount,
+                                    new BigDecimal(0.567585));
+                } else if (BankOperationController.currencyEUR.
+                        equals(changeCurrency)) {
+                    changeAmount = currencyConversion.
+                            convertFromCurrencyToAnother(changeAmount,
+                                    new BigDecimal(1.50));
+                }
+            }
+        } else if (BankOperationController.currencyEUR.
+                equals(accountCurrency)) {
+            bankOperation.setCurrency(client,
+                    BankOperationController.currencyEUR);
+            httpSession.setAttribute("selectedbgn", "");
+            httpSession.setAttribute("selectedusd", "");
+            httpSession.setAttribute("selectedeur", "selected");
+            
+            if (!accountCurrency.equals(changeCurrency)) {
+                if (BankOperationController.currencyBGN.
+                        equals(changeCurrency)) {
+                    changeAmount = currencyConversion.
+                            convertFromCurrencyToAnother(changeAmount,
+                                    new BigDecimal(0.511292)); //fixed exchange rate
+                } else if (BankOperationController.currencyUSD.
+                        equals(changeCurrency)) {
+                    changeAmount = currencyConversion.
+                            convertFromCurrencyToAnother(changeAmount,
+                                    new BigDecimal(1.50));
+                }
+            }
         }
         
         if (operation.equals("deposit")) {
@@ -98,17 +166,8 @@ public class BankOperationController extends HttpServlet {
                     + "less than 50 % of the current amount!");
         }
         
-        if (accountCurrency.equals("bgn")) {
-            httpSession.setAttribute("selectedbgn", "selected");
-        } else if (accountCurrency.equals("usd")) {
-            httpSession.setAttribute("selectedusd", "selected");
-        } else if (accountCurrency.equals("eur")) {
-            httpSession.setAttribute("selectedeur", "selected");
-        }
-        
         httpSession.setAttribute("id", client);
         httpSession.setAttribute("currentamount", currentAmount);
-        httpSession.setAttribute("accountcurrency", accountCurrency);
         
         httpResponse.sendRedirect("pages/Webbankingpage.jsp");
     }
