637589538175cd0f2f299cedacf074737f0cd6ea
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-15 00:40:31 +0200

    add UserData EJB

---
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/UserData.java
@@ -0,0 +1,11 @@
+package com.jeewd.ejbs;
+
+import java.util.Map;
+
+import javax.ejb.Local;
+
+@Local
+public interface UserData {
+    Map<String, Account> getUsersWithAccountsList();
+    void setUsersWithAccountsList(Map<String, Account> usersWithAccountsList);
+}
---
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/UserDataImpl.java
@@ -0,0 +1,25 @@
+package com.jeewd.ejbs;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+
+@Singleton
+public class UserDataImpl implements UserData {
+    private Map<String, Account> usersWithAccountsList;
+    
+    @PostConstruct
+    void postConstruct() {
+        setUsersWithAccountsList(new HashMap<>());
+    }
+
+    public Map<String, Account> getUsersWithAccountsList() {
+        return usersWithAccountsList;
+    }
+
+    public void setUsersWithAccountsList(Map<String,
+            Account> usersWithAccountsList) {
+        this.usersWithAccountsList = usersWithAccountsList;
+    }
+}
