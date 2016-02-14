4f3d857a4281627c8fd95705c3452e31d076e78e
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-14 17:02:41 +0200

    remove getDateAndTimeAsObject()

--- 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/CurrencyConversionImpl.java
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/CurrencyConversionImpl.java
@@ -1,26 +1,10 @@
 package com.jeewd.ejbs;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import javax.ejb.Stateless;
 
 @Stateless
 public class CurrencyConversionImpl implements CurrencyConversion {
-    private Date getDateAndTimeAsObject(String date) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
-                "dd.MM.yyyy HH:mm");
-        Date dateObject = null;
-        
-        try {
-            dateObject = simpleDateFormat.parse(date);
-        } catch (java.text.ParseException pe) {
-            pe.printStackTrace();
-        }
-        
-        return dateObject;
-    }
-
     @Override
     public BigDecimal convertFromCurrencyToAnother(BigDecimal fromValue,
             BigDecimal exchangeRate) {
