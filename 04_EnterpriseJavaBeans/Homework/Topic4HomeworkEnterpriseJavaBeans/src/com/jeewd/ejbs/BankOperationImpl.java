922600bd1dda1ec776efa17d30a3f29f7eb0b0c1
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-15 07:03:27 +0200

    simple withdraw (<= 50 % of current amount)

--- 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/BankOperationImpl.java
+++ 04_EnterpriseJavaBeans/Homework/Topic4HomeworkEnterpriseJavaBeans/src/com/jeewd/ejbs/BankOperationImpl.java
@@ -1,20 +1,10 @@
 package com.jeewd.ejbs;
 
 import java.math.BigDecimal;
-import javax.annotation.PostConstruct;
 import javax.ejb.Stateful;
 
 @Stateful
 public class BankOperationImpl implements BankOperation {
-    private BigDecimal allowedWithdrawMaxAmount;
-    private BigDecimal withdrawSoFar;
-    
-    @PostConstruct
-    void PostConstruct() {
-        this.allowedWithdrawMaxAmount = new BigDecimal(0).setScale(2);
-        this.withdrawSoFar = new BigDecimal(0).setScale(2);
-    }
-    
     @Override
     public BigDecimal deposit(BigDecimal currentAmount,
             BigDecimal changeAmount) {
@@ -41,17 +31,14 @@ public class BankOperationImpl implements BankOperation {
         BigDecimal changeAmountScaled = changeAmount.setScale(2,
                 BigDecimal.ROUND_HALF_UP);
         BigDecimal result = new BigDecimal(0).setScale(2);
-        this.withdrawSoFar = withdrawSoFar.add(changeAmountScaled);
-        this.allowedWithdrawMaxAmount = currentAmount.multiply(new BigDecimal(
-                0.50));
+        BigDecimal allowedWithdrawMaxAmount = currentAmount.multiply(
+                new BigDecimal(0.50));
         
         if ((changeAmountScaled.compareTo(result) == 1) &&
-                (this.withdrawSoFar.compareTo(this.allowedWithdrawMaxAmount)
-                        < 1)) {
+                (changeAmountScaled.compareTo(allowedWithdrawMaxAmount) < 1)) {
             result = currentAmountScaled.subtract(changeAmountScaled);
         } else {
             result = currentAmountScaled;
-            this.withdrawSoFar = withdrawSoFar.subtract(changeAmountScaled);
         }
         
         return result;
