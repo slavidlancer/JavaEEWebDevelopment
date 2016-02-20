79f6a671091c4b2f03921652903c2f876cc7621e
slavidlancer <slavidlancer@users.noreply.github.com>
2016-02-20 19:26:41 +0200

    add URL constants

---
+++ 06_SpringMVC/SpringMVCUniversityExampleClass/src/main/java/com/jeews/university/constants/UrlConstants.java
@@ -0,0 +1,6 @@
+package com.jeews.university.constants;
+
+public class UrlConstants {
+    public static final String STUDENT_REGISTER_URL = "/getStudents";
+    public static final String STUDENT_ADD_STUDENT_URL = "/insertStudent";
+}
