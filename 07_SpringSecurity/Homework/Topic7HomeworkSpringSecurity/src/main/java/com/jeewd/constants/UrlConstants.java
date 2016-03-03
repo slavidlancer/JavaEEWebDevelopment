7ec3c49489e77092fa22f8d9b5c78b6740daa2c3
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-03 18:52:11 +0200

    update UrlConstants in Topic7HomeworkSpringSecurity project

--- 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/constants/UrlConstants.java
+++ 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/constants/UrlConstants.java
@@ -1,6 +1,9 @@
 package com.jeewd.constants;
 
 public class UrlConstants {
-    private static final String BANK_OPERATION_URL = "/bankOp";
-    private static final String BANK_ACCOUNT_URL = "/bankAcc";
+    public static final String BANK_REGISTER_PAGE_URL = "/bankRegister";
+    public static final String ACCOUNT_CREATION_PAGE_URL = "/createAccount";
+    public static final String ADD_ACCOUNT_URL = "/addAccount";
+    public static final String OPERATION_PAGE_URL = "/operation";
+    public static final String PROCESS_OPERATION_URL = "/processOperation";
 }
