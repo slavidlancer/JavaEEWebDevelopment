af45f4e2e25dd01b2521faa420107704b1642c8c
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-03 17:02:56 +0200

    add UrlConstants

---
+++ 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/constants/UrlConstants.java
@@ -0,0 +1,6 @@
+package com.jeewd.constants;
+
+public class UrlConstants {
+    private static final String BANK_OPERATION_URL = "/bankOp";
+    private static final String BANK_ACCOUNT_URL = "/bankAcc";
+}
