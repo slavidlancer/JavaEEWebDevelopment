c3e70a01e9d61f38b66bb9aa0eb444d07cf9cc8b
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-06 00:17:02 +0200

    use BankAccount and createdBy user, getAllAccounts()

--- 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/securebank/services/AccountService.java
+++ 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/securebank/services/AccountService.java
@@ -1,14 +1,9 @@
 package com.jeewd.securebank.services;
 
-import java.math.BigDecimal;
 import java.util.List;
-
 import com.jeewd.securebank.entity.BankAccount;
-import com.jeewd.securebank.entity.CurrencyID;
 
 public interface AccountService {
-    boolean addAccount(String username, String number, BigDecimal amount,
-            CurrencyID currency);
-
-    List<BankAccount> getAccounts(String username);
+    boolean addAccount(BankAccount bankAccount, String user);
+    List<BankAccount> getAllAccounts();
 }
