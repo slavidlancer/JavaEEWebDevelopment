4380cfc90214a2d3aa8e28de58684ab7cda107a7
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-06 00:20:50 +0200

    use / and Bankregisterpage URLs in one method, use AccountService with @Autowired, add UrlConstants in every redirection, add user.username info in every redirection, proper add and get accounts

--- 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/securebank/controllers/BankController.java
+++ 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/securebank/controllers/BankController.java
@@ -1,6 +1,7 @@
 package com.jeewd.securebank.controllers;
 
-import javax.inject.Inject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +16,32 @@ import com.jeewd.securebank.utils.UserUtils;
 
 @Controller
 public class BankController {
-    @Inject
+    @Autowired
+    @Qualifier("accountServiceImpl")
     private AccountService accountService;
     
     @Secured({"ROLE_USER", "ROLE_BANK_EMPLOYEE"})
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String goToDefaultPage(Model model) {
+    @RequestMapping(value = {"/", UrlConstants.BANK_REGISTER_PAGE_URL},
+            method = RequestMethod.GET)
+    public String goToBankRegister(Model model) {
+        model.addAttribute("bankRegisterUrl",
+                UrlConstants.BANK_REGISTER_PAGE_URL);
+        model.addAttribute("createAccountUrl",
+                UrlConstants.ACCOUNT_CREATION_PAGE_URL);
+        model.addAttribute("operationUrl", UrlConstants.OPERATION_PAGE_URL);
+        model.addAttribute("addAccountUrl", UrlConstants.ADD_ACCOUNT_URL);
+        model.addAttribute("processOperationUrl",
+                UrlConstants.PROCESS_OPERATION_URL);
+        model.addAttribute("user", UserUtils.getUser());
+        model.addAttribute("accounts", accountService.getAllAccounts());
+        
+        return "Bankregisterpage";
+    }
+    
+    @Secured("ROLE_BANK_EMPLOYEE")
+    @RequestMapping(value = UrlConstants.ACCOUNT_CREATION_PAGE_URL,
+            method = RequestMethod.GET)
+    public String goToAccountCreation(Model model) {
         model.addAttribute("bankRegisterUrl",
                 UrlConstants.BANK_REGISTER_PAGE_URL);
         model.addAttribute("createAccountUrl",
@@ -31,20 +52,6 @@ public class BankController {
                 UrlConstants.PROCESS_OPERATION_URL);
         model.addAttribute("user", UserUtils.getUser());
         
-        return "Bankregisterpage";
-    }
-    
-    @Secured({"ROLE_USER", "ROLE_BANK_EMPLOYEE"})
-    @RequestMapping(value = UrlConstants.BANK_REGISTER_PAGE_URL,
-            method = RequestMethod.GET)
-    public String goToBankRegister(Model model) {
-        return "Bankregisterpage";
-    }
-    
-    @Secured("ROLE_BANK_EMPLOYEE")
-    @RequestMapping(value = UrlConstants.ACCOUNT_CREATION_PAGE_URL,
-            method = RequestMethod.GET)
-    public String goToAccountCreation(Model model) {
         return "Accountcreation";
     }
     
@@ -52,19 +59,36 @@ public class BankController {
     @RequestMapping(value = UrlConstants.OPERATION_PAGE_URL,
             method = RequestMethod.GET)
     public String goToOperation(Model model) {
+        model.addAttribute("bankRegisterUrl",
+                UrlConstants.BANK_REGISTER_PAGE_URL);
+        model.addAttribute("createAccountUrl",
+                UrlConstants.ACCOUNT_CREATION_PAGE_URL);
+        model.addAttribute("operationUrl", UrlConstants.OPERATION_PAGE_URL);
+        model.addAttribute("addAccountUrl", UrlConstants.ADD_ACCOUNT_URL);
+        model.addAttribute("processOperationUrl",
+                UrlConstants.PROCESS_OPERATION_URL);
+        model.addAttribute("user", UserUtils.getUser());
+        
         return "Operation";
     }
     
     @Secured("ROLE_BANK_EMPLOYEE")
     @RequestMapping(value = UrlConstants.ADD_ACCOUNT_URL,
             method = RequestMethod.POST)
-    public String addAccount(Model model, @ModelAttribute("bankAccount")
+    public String addAccount(Model model, @ModelAttribute(value = "bankAccount")
             BankAccount bankAccount) {
-        /*accountService.addAccount(bankAccount.getUsername(),
-                bankAccount.getNumber(), bankAccount.getAmount(),
-                bankAccount.getCurrency());
-        model.addAttribute("accounts", accountService.getAccounts(bankAccount.
-                getUsername()));*/
+        String user = UserUtils.getUser().getUsername();
+        model.addAttribute("bankRegisterUrl",
+                UrlConstants.BANK_REGISTER_PAGE_URL);
+        model.addAttribute("createAccountUrl",
+                UrlConstants.ACCOUNT_CREATION_PAGE_URL);
+        model.addAttribute("operationUrl", UrlConstants.OPERATION_PAGE_URL);
+        model.addAttribute("addAccountUrl", UrlConstants.ADD_ACCOUNT_URL);
+        model.addAttribute("processOperationUrl",
+                UrlConstants.PROCESS_OPERATION_URL);
+        accountService.addAccount(bankAccount, user);
+        model.addAttribute("user", UserUtils.getUser());
+        model.addAttribute("accounts", accountService.getAllAccounts());
         
         return "Bankregisterpage";
     }
@@ -74,6 +98,17 @@ public class BankController {
             method = RequestMethod.POST)
     public String processOperation(Model model, @ModelAttribute("bankOperation")
             BankOperation bankOperation) {
+        model.addAttribute("bankRegisterUrl",
+                UrlConstants.BANK_REGISTER_PAGE_URL);
+        model.addAttribute("createAccountUrl",
+                UrlConstants.ACCOUNT_CREATION_PAGE_URL);
+        model.addAttribute("operationUrl", UrlConstants.OPERATION_PAGE_URL);
+        model.addAttribute("addAccountUrl", UrlConstants.ADD_ACCOUNT_URL);
+        model.addAttribute("processOperationUrl",
+                UrlConstants.PROCESS_OPERATION_URL);
+        model.addAttribute("user", UserUtils.getUser());
+        model.addAttribute("accounts", accountService.getAllAccounts());
+        
         return "Bankregisterpage";
     }
 }
