dc6e8147298d2483774241d830af2880c007d8fb
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-05 09:25:44 +0200

    remove currently not needed comments in the BankController, refactor

--- 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/securebank/controllers/BankController.java
+++ 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/securebank/controllers/BankController.java
@@ -10,44 +10,24 @@ import com.jeewd.constants.UrlConstants;
 public class BankController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String goToDefaultPage(Model model) {
-        //model.addAttribute("students", studentService.getStudents());
-        /*model.addAttribute("addStudentUrl",
-                UrlConstants.ADD_STUDENT_URL);*/
-        //model.addAttribute("user", UserUtils.getUser());
-        
         return "Bankregisterpage";
     }
     
     @RequestMapping(value = UrlConstants.BANK_REGISTER_PAGE_URL,
             method = RequestMethod.GET)
     public String goToBankRegister(Model model) {
-        //model.addAttribute("students", studentService.getStudents());
-        /*model.addAttribute("addStudentUrl",
-                UrlConstants.ADD_STUDENT_URL);*/
-        //model.addAttribute("user", UserUtils.getUser());
-        
         return "Bankregisterpage";
     }
     
     @RequestMapping(value = UrlConstants.ACCOUNT_CREATION_PAGE_URL,
             method = RequestMethod.GET)
     public String goToAccountCreation(Model model) {
-        //model.addAttribute("students", studentService.getStudents());
-        /*model.addAttribute("addStudentUrl",
-                UrlConstants.ADD_STUDENT_URL);*/
-        //model.addAttribute("user", UserUtils.getUser());
-        
         return "Accountcreation";
     }
     
     @RequestMapping(value = UrlConstants.OPERATION_PAGE_URL,
             method = RequestMethod.GET)
     public String goToOperation(Model model) {
-        //model.addAttribute("students", studentService.getStudents());
-        /*model.addAttribute("addStudentUrl",
-                UrlConstants.ADD_STUDENT_URL);*/
-        //model.addAttribute("user", UserUtils.getUser());
-        
         return "Operation";
     }
 }
