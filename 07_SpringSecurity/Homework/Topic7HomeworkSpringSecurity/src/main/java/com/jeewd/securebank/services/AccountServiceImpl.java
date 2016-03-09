4c2fb3bae5acf79dcd4211512aba4e2df4043a98
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-09 12:24:12 +0200

    remove not needed map with username - bank accounts and its usage

--- 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/securebank/services/AccountServiceImpl.java
+++ 07_SpringSecurity/Homework/Topic7HomeworkSpringSecurity/src/main/java/com/jeewd/securebank/services/AccountServiceImpl.java
@@ -1,6 +1,5 @@
 package com.jeewd.securebank.services;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import org.springframework.stereotype.Service;
@@ -8,8 +7,6 @@ import com.jeewd.securebank.entity.BankAccount;
 
 @Service
 public class AccountServiceImpl implements AccountService {
-    private static HashMap<String, Set<BankAccount>> database =
-            new HashMap<>();
     private static Set<BankAccount> bankAccounts = new HashSet<>();
     
     @Override
@@ -18,7 +15,6 @@ public class AccountServiceImpl implements AccountService {
         
         if (!bankAccounts.contains(bankAccount)) {
             bankAccounts.add(bankAccount);
-            database.put(bankAccount.getUsername(), bankAccounts);
         }
         
         return true;
