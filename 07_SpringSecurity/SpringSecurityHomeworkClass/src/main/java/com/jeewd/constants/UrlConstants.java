2162535db51e090d3ba2c5ca49b9fca37ba2438e
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-12 12:43:00 +0200

    remove default HomeController and home.jsp, remove everything which is Student related (controller, entitiy, services), remove UrlConstants, remove old servlet and root contexts

--- 07_SpringSecurity/SpringSecurityHomeworkClass/src/main/java/com/jeewd/constants/UrlConstants.java
+++
@@ -1,7 +0,0 @@
-package com.jeewd.constants;
-
-public class UrlConstants {
-    public static final String STUDENT_REGISTER_URL = "/studentsRegister";
-    public static final String ADD_STUDENT_URL = "/addStudent";
-    public static final String ADD_STUDENT_SAVE_URL = "/addStudentSave";
-}
