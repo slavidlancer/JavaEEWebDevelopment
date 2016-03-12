c51bf93efc04678cf8201b9968e77d8a1370a0ef
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-12 13:00:45 +0200

    refactor BankOperation DTO

--- 07_SpringSecurity/SpringSecurityHomeworkClass/src/main/java/com/jeewd/secure_bank/dto/BankOperation.java
+++ 07_SpringSecurity/SpringSecurityHomeworkClass/src/main/java/com/jeewd/secure_bank/dto/BankOperation.java
@@ -11,24 +11,31 @@ public class BankOperation {
     public String getClient() {
         return client;
     }
+    
     public void setClient(String client) {
         this.client = client;
     }
+    
     public String getOperation() {
         return operation;
     }
+    
     public void setOperation(String operation) {
         this.operation = operation;
     }
+    
     public BigDecimal getAmount() {
         return amount;
     }
+    
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
     }
+    
     public String getCurrency() {
         return currency;
     }
+    
     public void setCurrency(String currency) {
         this.currency = currency;
     }
