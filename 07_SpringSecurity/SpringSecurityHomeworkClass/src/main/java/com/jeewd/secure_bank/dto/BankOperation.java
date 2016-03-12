53220b2326c0a916b1aac093d405a360733f1c30
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-12 12:53:32 +0200

    add everything which is Bank related (controller, DTO, entitiy, services)

---
+++ 07_SpringSecurity/SpringSecurityHomeworkClass/src/main/java/com/jeewd/secure_bank/dto/BankOperation.java
@@ -0,0 +1,35 @@
+package com.jeewd.secure_bank.dto;
+
+import java.math.BigDecimal;
+
+public class BankOperation {
+    private String client;
+    private String operation;
+    private BigDecimal amount;
+    private String currency;
+    
+    public String getClient() {
+        return client;
+    }
+    public void setClient(String client) {
+        this.client = client;
+    }
+    public String getOperation() {
+        return operation;
+    }
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+    public String getCurrency() {
+        return currency;
+    }
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
