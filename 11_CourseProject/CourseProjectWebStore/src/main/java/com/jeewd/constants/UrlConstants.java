f9c14c0471bf72b3ff75134bd5149651fb4afa82
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-13 22:50:42 +0200

    add example controllers (login, from bank example), user security, user utils, example constants structure

---
+++ 11_CourseProject/CourseProjectWebStore/src/main/java/com/jeewd/constants/UrlConstants.java
@@ -0,0 +1,9 @@
+package com.jeewd.constants;
+
+public class UrlConstants {
+    /*public static final String BANK_REGISTER_PAGE_URL = "/bankRegister";
+    public static final String ACCOUNT_CREATION_PAGE_URL = "/createAccount";
+    public static final String ADD_ACCOUNT_URL = "/addAccount";
+    public static final String OPERATION_PAGE_URL = "/operation";
+    public static final String PROCESS_OPERATION_URL = "/processOperation";*/
+}
---
+++ 11_CourseProject/CourseProjectWebStore/src/main/java/com/jeewd/web_store/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package com.jeewd.web_store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//http://localhost:8080/web_store
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage() {
+        return "Login";
+    }
+}
