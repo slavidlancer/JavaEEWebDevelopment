7896f56fe5c5d614c753fa4a9659079b9494d622
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-14 16:25:10 +0200

    improve BankAccountDao, add/get by username and number/get all bank accounts

--- 08_OracleDatabase/Homework/Topic8HomeworkSpringJDBCOracleDatabase/src/main/java/com/jeewd/jdbc_bank/dao/BankAccountDaoImpl.java
+++ 08_OracleDatabase/Homework/Topic8HomeworkSpringJDBCOracleDatabase/src/main/java/com/jeewd/jdbc_bank/dao/BankAccountDaoImpl.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import org.springframework.stereotype.Repository;
 import com.jeewd.constants.DbConstants;
 import com.jeewd.jdbc_bank.entity.BankAccount;
+import com.jeewd.jdbc_bank.entity.CurrencyID;
 
 @Repository
 public class BankAccountDaoImpl implements BankAccountDao {
@@ -24,17 +25,27 @@ public class BankAccountDaoImpl implements BankAccountDao {
     
     @Override
     public boolean addBankAccount(BankAccount bankAccount, String user) {
-        String sql = "INSERT INTO accounts (, , ....) VALUES (?, ?, ?)";
+        String sqlInsert = "INSERT INTO accounts (id, account_number, username,"
+                + " amount, currency, created_by) VALUES (?, ?, ?, ?, ?, ?)";
         
         try (Connection connection = DriverManager.getConnection(
                 DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
-                PreparedStatement statement =
-                        connection.prepareStatement(sql);) {
-            /*statement.setLong(1, 2);
-            statement.setString(2, student.getFacultyNumber());
-            statement.setString(3, student.getName());*/
+                Statement statement = connection.createStatement();
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(sqlInsert);) {
+            String sqlRetrieve = "SELECT LAST_INSERT_ID() FROM operations";
+            ResultSet resultSet = statement.executeQuery(sqlRetrieve);
+            long lastId = Long.parseLong(resultSet.getString(1));
             
-            statement.executeQuery();
+            preparedStatement.setLong(1, ++lastId);
+            preparedStatement.setString(2, bankAccount.getNumber());
+            preparedStatement.setString(3, bankAccount.getUsername());
+            preparedStatement.setBigDecimal(4, bankAccount.getAmount());
+            preparedStatement.setObject(5, bankAccount.getCurrency().
+                    toString());
+            preparedStatement.setString(6, bankAccount.getCreatedBy());
+            
+            preparedStatement.executeQuery();
         } catch (SQLException sqle) {
             sqle.printStackTrace();
             
@@ -48,20 +59,49 @@ public class BankAccountDaoImpl implements BankAccountDao {
     public BankAccount getBankAccountNumberByUsername(String username,
             String number) {
         BankAccount bankAccount = null;
+        String sql = "SELECT * FROM accounts WHERE username = '?' AND "
+                + "account_number = '?'";
         
         try (Connection connection = DriverManager.getConnection(
                 DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
-                Statement statement = connection.createStatement();) {
-            String sql = "SELECT * FROM accounts";
-            ResultSet resultSet = statement.executeQuery(sql);
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, number);
             
-            while (resultSet.next()) {
-                bankAccount = new BankAccount();
+            ResultSet resultSet = preparedStatement.executeQuery(sql);
+            
+            bankAccount = new BankAccount();
+            CurrencyID currency = null;
                 
-                /*student.setId(resultSet.getLong("id"));
-                student.setFacultyNumber(resultSet.getString("faculty_number"));
-                student.setName(resultSet.getString("name"));*/
+            bankAccount.setNumber(resultSet.getString(2));
+            bankAccount.setUsername(resultSet.getString(3));
+            bankAccount.setAmount(resultSet.getBigDecimal(4));
+            
+            switch (resultSet.getString(5)) {
+                case "BGN":
+                    currency = CurrencyID.BGN;
+                    
+                    break;
+                    
+                case "USD":
+                    currency = CurrencyID.USD;
+                    
+                    break;
+                    
+                case "EUR":
+                    currency = CurrencyID.EUR;
+                    
+                    break;
+                    
+                default:
+                    currency = CurrencyID.BGN;
+                    
+                    break;
             }
+            
+            bankAccount.setCurrency(currency);
+            bankAccount.setCreatedBy(resultSet.getString(6));
         } catch (SQLException sqle) {
             sqle.printStackTrace();
             
@@ -83,10 +123,36 @@ public class BankAccountDaoImpl implements BankAccountDao {
             
             while (resultSet.next()) {
                 BankAccount bankAccount = new BankAccount();
+                CurrencyID currency = null;
                 
-                /*student.setId(resultSet.getLong("id"));
-                student.setFacultyNumber(resultSet.getString("faculty_number"));
-                student.setName(resultSet.getString("name"));*/
+                bankAccount.setNumber(resultSet.getString(2));
+                bankAccount.setUsername(resultSet.getString(3));
+                bankAccount.setAmount(resultSet.getBigDecimal(4));
+                
+                switch (resultSet.getString(5)) {
+                    case "BGN":
+                        currency = CurrencyID.BGN;
+                    
+                        break;
+                        
+                    case "USD":
+                        currency = CurrencyID.USD;
+                    
+                        break;
+                        
+                    case "EUR":
+                        currency = CurrencyID.EUR;
+                    
+                        break;
+
+                    default:
+                        currency = CurrencyID.BGN;
+                        
+                        break;
+                }
+                
+                bankAccount.setCurrency(currency);
+                bankAccount.setCreatedBy(resultSet.getString(6));
                 
                 bankAccounts.add(bankAccount);
             }
