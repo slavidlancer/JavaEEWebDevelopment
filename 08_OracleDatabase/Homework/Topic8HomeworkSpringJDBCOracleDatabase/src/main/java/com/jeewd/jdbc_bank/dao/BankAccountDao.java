397267d420a6c17df23fb1fe31c4eb50fe24e5b2
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-14 15:58:10 +0200

    improve BankAccountDao, refactor

--- 08_OracleDatabase/Homework/Topic8HomeworkSpringJDBCOracleDatabase/src/main/java/com/jeewd/jdbc_bank/dao/BankAccountDao.java
+++ 08_OracleDatabase/Homework/Topic8HomeworkSpringJDBCOracleDatabase/src/main/java/com/jeewd/jdbc_bank/dao/BankAccountDao.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import com.jeewd.jdbc_bank.entity.BankAccount;
 
 public interface BankAccountDao {
-    Set<BankAccount> getBankAccounts();
-    boolean addBankAccount(BankAccount bankAccount);
+    boolean addBankAccount(BankAccount bankAccount, String user);
+    BankAccount getBankAccountNumberByUsername(String username, String number);
+    Set<BankAccount> getAllBankAccounts();
 }
--- 08_OracleDatabase/Homework/Topic8HomeworkSpringJDBCOracleDatabase/src/main/java/com/jeewd/jdbc_bank/dao/BankAccountDaoImpl.java
+++ 08_OracleDatabase/Homework/Topic8HomeworkSpringJDBCOracleDatabase/src/main/java/com/jeewd/jdbc_bank/dao/BankAccountDaoImpl.java
@@ -9,14 +9,11 @@ import java.sql.Statement;
 import java.util.HashSet;
 import java.util.Set;
 import org.springframework.stereotype.Repository;
+import com.jeewd.constants.DbConstants;
 import com.jeewd.jdbc_bank.entity.BankAccount;
 
 @Repository
 public class BankAccountDaoImpl implements BankAccountDao {
-    public static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
-    public static final String USERNAME = "University";
-    public static final String PASSWORD = "University";
-    
     static {
         try {
             Class.forName("oracle.jdbc.OracleDriver");
@@ -24,15 +21,64 @@ public class BankAccountDaoImpl implements BankAccountDao {
             cnfe.printStackTrace();
         }
     }
+    
+    @Override
+    public boolean addBankAccount(BankAccount bankAccount, String user) {
+        String sql = "INSERT INTO accounts (, , ....) VALUES (?, ?, ?)";
+        
+        try (Connection connection = DriverManager.getConnection(
+                DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
+                PreparedStatement statement =
+                        connection.prepareStatement(sql);) {
+            /*statement.setLong(1, 2);
+            statement.setString(2, student.getFacultyNumber());
+            statement.setString(3, student.getName());*/
+            
+            statement.executeQuery();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            
+            return false;
+        }
+        
+        return true;
+    }
+    
+    @Override
+    public BankAccount getBankAccountNumberByUsername(String username,
+            String number) {
+        BankAccount bankAccount = null;
+        
+        try (Connection connection = DriverManager.getConnection(
+                DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
+                Statement statement = connection.createStatement();) {
+            String sql = "SELECT * FROM accounts";
+            ResultSet resultSet = statement.executeQuery(sql);
+            
+            while (resultSet.next()) {
+                bankAccount = new BankAccount();
+                
+                /*student.setId(resultSet.getLong("id"));
+                student.setFacultyNumber(resultSet.getString("faculty_number"));
+                student.setName(resultSet.getString("name"));*/
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            
+            return null;
+        }
+        
+        return bankAccount;
+    }
 
     @Override
-    public Set<BankAccount> getBankAccounts() {
+    public Set<BankAccount> getAllBankAccounts() {
         Set<BankAccount> bankAccounts = new HashSet<>();
         
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
-                    PASSWORD);
+        try (Connection connection = DriverManager.getConnection(
+                DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
                 Statement statement = connection.createStatement();) {
-            String sql = "SELECT * FROM students";
+            String sql = "SELECT * FROM accounts";
             ResultSet resultSet = statement.executeQuery(sql);
             
             while (resultSet.next()) {
@@ -51,27 +97,5 @@ public class BankAccountDaoImpl implements BankAccountDao {
         }
         
         return bankAccounts;
-    }
-
-    @Override
-    public boolean addBankAccount(BankAccount bankAccount) {
-        String sql = "INSERT INTO accounts (, , ....) VALUES (?, ?, ?)";
-        
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME,
-                    PASSWORD);
-                PreparedStatement statement =
-                        connection.prepareStatement(sql);) {
-            /*statement.setLong(1, 2);
-            statement.setString(2, student.getFacultyNumber());
-            statement.setString(3, student.getName());*/
-            
-            statement.executeQuery();
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
-            
-            return false;
-        }
-        
-        return true;
     }
 }
