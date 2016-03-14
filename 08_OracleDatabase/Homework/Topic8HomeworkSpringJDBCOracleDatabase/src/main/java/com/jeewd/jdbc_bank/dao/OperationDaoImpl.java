26717a884da8a1516c04755bfecc8c36d039358f
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-14 14:57:32 +0200

    improve data insertion, register operation

--- 08_OracleDatabase/Homework/Topic8HomeworkSpringJDBCOracleDatabase/src/main/java/com/jeewd/jdbc_bank/dao/OperationDaoImpl.java
+++ 08_OracleDatabase/Homework/Topic8HomeworkSpringJDBCOracleDatabase/src/main/java/com/jeewd/jdbc_bank/dao/OperationDaoImpl.java
@@ -3,7 +3,9 @@ package com.jeewd.jdbc_bank.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import org.springframework.stereotype.Repository;
 import com.jeewd.constants.DbConstants;
 
@@ -20,21 +22,27 @@ public class OperationDaoImpl implements OperationDao {
     @Override
     public boolean registerOperation(String accountNumber, String operation,
             String amount, String currency, String performedBy) {
-        String sql = "INSERT INTO operations (id, account_umber, operation, "
-                + "amount, currency, performedBy) VALUES (?, ?, ?, ?, ?, ?)";
+        String sqlInsert = "INSERT INTO operations (id, account_umber,"
+                + "operation, amount, currency, performed_by) VALUES (?, ?, ?, "
+                + "?, ?, ?)";
         
         try (Connection connection = DriverManager.getConnection(
                 DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
-                PreparedStatement statement =
-                        connection.prepareStatement(sql);) {
-            statement.setLong(1, 2);
-            statement.setString(2, accountNumber);
-            statement.setString(3, operation);
-            statement.setString(4, amount);
-            statement.setString(5, currency);
-            statement.setString(6, performedBy);
+                Statement statement = connection.createStatement();
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(sqlInsert);) {
+            String sqlRetrieve = "SELECT LAST_INSERT_ID() FROM operations";
+            ResultSet resultSet = statement.executeQuery(sqlRetrieve);
+            long lastId = Long.parseLong(resultSet.getString(1));
             
-            statement.executeQuery();
+            preparedStatement.setLong(1, ++lastId);
+            preparedStatement.setString(2, accountNumber);
+            preparedStatement.setString(3, operation);
+            preparedStatement.setString(4, amount);
+            preparedStatement.setString(5, currency);
+            preparedStatement.setString(6, performedBy);
+            
+            preparedStatement.executeQuery();
         } catch (SQLException sqle) {
             sqle.printStackTrace();
             
