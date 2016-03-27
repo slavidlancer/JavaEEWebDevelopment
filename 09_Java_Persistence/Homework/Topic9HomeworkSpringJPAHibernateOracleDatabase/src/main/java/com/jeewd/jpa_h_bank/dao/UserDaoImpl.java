a3b21efbbca70d032517663daa79719aa160d276
slavidlancer <slavidlancer@users.noreply.github.com>
2016-03-27 23:23:02 +0300

    refactoring, data retrieval through Hibernate

--- 09_Java_Persistence/Homework/Topic9HomeworkSpringJPAHibernateOracleDatabase/src/main/java/com/jeewd/jpa_h_bank/dao/UserDaoImpl.java
+++ 09_Java_Persistence/Homework/Topic9HomeworkSpringJPAHibernateOracleDatabase/src/main/java/com/jeewd/jpa_h_bank/dao/UserDaoImpl.java
@@ -1,62 +1,86 @@
 package com.jeewd.jpa_h_bank.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.jeewd.jpa_h_bank.entities.UserDb;
 
 @Repository
 public class UserDaoImpl implements UserDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+    
     @Override
     public Map<Long, UserDb> getAllUsers() {
-        /*Map<Long, UserDb> users = new HashMap<>();
+        Map<Long, UserDb> users = new HashMap<>();
+        List<?> userDbList = new ArrayList<UserDb>();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
         
-        try (Connection connection = DriverManager.getConnection("", "", "");
-                //DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
-                Statement statement = connection.createStatement();) {
-            String sql = "SELECT * FROM users";
+        try {
+            transaction = session.beginTransaction();
+            String hql = "FROM com.jeewd.jpa_h_bank.entities.UserDb";
+            Query query = session.createQuery(hql);
             
-            ResultSet resultSet = statement.executeQuery(sql);
+            userDbList = query.list();
             
-            while (resultSet.next()) {
-                UserDb userDb = new UserDb();
-                
-                userDb.setId(resultSet.getLong(1));
-                userDb.setUsername(resultSet.getString(2));
-                userDb.setPassword(resultSet.getString(3));
-                userDb.setRole(resultSet.getString(4));
-                
+            for (Object object : userDbList) {
+                UserDb userDb = (UserDb) object;
                 users.put(userDb.getId(), userDb);
             }
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
             
-            return null;
-        }*/
+            transaction.commit();
+        } catch (HibernateException he) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            
+            he.printStackTrace();
+        } finally {
+            session.close();
+        }
         
-        return null;
+        return users;
     }
 
     @Override
     public List<String> getAllUsernames() {
-        /*List<String> usernames = new ArrayList<>();
+        List<String> usernames = new ArrayList<>();
+        List<?> userDbList = new ArrayList<UserDb>();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
         
-        try (Connection connection = DriverManager.getConnection("", "", "");
-                //DbConstants.URL, DbConstants.USERNAME, DbConstants.PASSWORD);
-                Statement statement = connection.createStatement();) {
-            String sql = "SELECT * FROM users";
+        try {
+            transaction = session.beginTransaction();
+            String hql = "FROM com.jeewd.jpa_h_bank.entities.UserDb";
+            Query query = session.createQuery(hql);
             
-            ResultSet resultSet = statement.executeQuery(sql);
+            userDbList = query.list();
             
-            while (resultSet.next()) {
-                usernames.add(resultSet.getString(2));
+            for (Object object : userDbList) {
+                UserDb userDb = (UserDb) object;
+                usernames.add(userDb.getUsername());
             }
-        } catch (SQLException sqle) {
-            sqle.printStackTrace();
             
-            return null;
-        }*/
+            transaction.commit();
+        } catch (HibernateException he) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            
+            he.printStackTrace();
+        } finally {
+            session.close();
+        }
         
-        return null;
+        return usernames;
     }
 }
