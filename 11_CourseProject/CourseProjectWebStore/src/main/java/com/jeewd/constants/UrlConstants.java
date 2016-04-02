dec786c5e95fd53a0c2eb6acda8b5a272a150dce
slavidlancer <slavidlancer@users.noreply.github.com>
2016-04-02 23:27:18 +0300

    update UrlConstants and reorganize views (customer, product, order)

--- 11_CourseProject/CourseProjectWebStore/src/main/java/com/jeewd/constants/UrlConstants.java
+++ 11_CourseProject/CourseProjectWebStore/src/main/java/com/jeewd/constants/UrlConstants.java
@@ -2,17 +2,17 @@ package com.jeewd.constants;
 
 public class UrlConstants {
     public static final String PRODUCT_REGISTRY_PAGE_URL =
-            "/product/ProductRegistry";
+            "/ProductRegistry";
     public static final String PRODUCT_ADD_EDIT_PAGE_URL =
-            "/product/AddEditProduct";
+            "/AddEditProduct";
     public static final String CUSTOMER_REGISTRY_PAGE_URL =
-            "/customer/CustomerRegistry";
+            "/CustomerRegistry";
     public static final String CUSTOMER_ADD_EDIT_PAGE_URL =
-            "/customer/AddEditCustomer";
+            "/AddEditCustomer";
     public static final String ORDER_REGISTRY_PAGE_URL =
-            "/order/OrderRegistry";
+            "/OrderRegistry";
     public static final String ORDER_ADD_EDIT_PAGE_URL =
-            "/order/AddEditOrder";
+            "/AddEditOrder";
     /*public static final String ADD_ACCOUNT_URL = "/addAccount";
     public static final String OPERATION_PAGE_URL = "/operation";
     public static final String PROCESS_OPERATION_URL = "/processOperation";*/
