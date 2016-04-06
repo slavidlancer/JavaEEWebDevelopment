9f166542ce558ca9fc02ccc84848d7da6cd5d130
slavidlancer <slavidlancer@users.noreply.github.com>
2016-04-07 00:11:43 +0300

    add DTOs for searching (product, customer, order, user) (initial configurations (templates))

---
+++ 11_CourseProject/CourseProjectWebStore/src/main/java/com/jeewd/web_store/dto/order/OrderSearch.java
@@ -0,0 +1,40 @@
+package com.jeewd.web_store.dto.order;
+
+public class OrderSearch {
+    private String username;
+    private String name;
+    private String type;
+    private String status;
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
