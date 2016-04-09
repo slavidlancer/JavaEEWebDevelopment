88a1078660506308a4a44a1aa962fbf42cf1edeb
slavidlancer <slavidlancer@users.noreply.github.com>
2016-04-09 14:17:39 +0300

    proper treatment if user is null (wrong, not active, etc.) in security

--- 11_CourseProject/CourseProjectWebStore/src/main/java/com/jeewd/web_store/security/UserDetailsServiceImpl.java
+++ 11_CourseProject/CourseProjectWebStore/src/main/java/com/jeewd/web_store/security/UserDetailsServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import com.jeewd.web_store.dao.user.UserDao;
 import com.jeewd.web_store.entities.user.Role;
 import com.jeewd.web_store.entities.user.User;
 import com.jeewd.web_store.services.user.UserService;
@@ -24,11 +23,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         
         List<GrantedAuthority> roles = new ArrayList<>();
         
-        for (Role role : user.getRoles()) {
-            roles.add(new SimpleGrantedAuthority(role.getRole()));
+        if (user != null) {
+            for (Role role : user.getRoles()) {
+                roles.add(new SimpleGrantedAuthority(role.getRole()));
+            }
+            
+            return new com.jeewd.web_store.security.User(user.getUsername(),
+                    user.getPassword(), roles);
+        } else {
+            return new com.jeewd.web_store.security.User("", "",
+                    new ArrayList<GrantedAuthority>());
         }
-        
-        return new com.jeewd.web_store.security.User(user.getUsername(),
-                user.getPassword(), roles);
     }
 }
