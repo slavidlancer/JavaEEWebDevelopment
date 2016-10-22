7dc4e62afb2875c4dbb84b7dacce9379f6732739
slavidlancer <slavidlancer@users.noreply.github.com>
2016-10-22 21:16:11 +0300

    add new Enterprise Application Projects from Java EE Fundamentals Course at SoftUni, Java EE Basics, Servlet, JSP, JSF, initial commit

---
+++ 13_Servlet_JSP_JSF/01/Project02Web/src/com/jee/web/LoginBean.java
@@ -0,0 +1,73 @@
+package com.jee.web;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+
+@ManagedBean(name = "loginBean")
+@RequestScoped
+public class LoginBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Inject
+    private HttpServletRequest request;
+    
+    private String username;
+    private String password;
+    
+    private static final String SUCCESS_LOGIN_REDIRECT = "/page/success?faces-redirect=true";
+    private static final String LOGIN_PAGE_REDIRECT = "/page/login?faces-redirect=true";
+    
+    @PostConstruct
+    public void init() {}
+    
+    public String login() {
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("invalid credentials"));
+            
+            return "";
+        } else if ("admin".equals(username) && "123".equals(password)) {
+            /*HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().
+                    getExternalContext().getRequest();*/
+            request.getSession().setAttribute("LOGGED_USER", username);
+            
+            return SUCCESS_LOGIN_REDIRECT;
+        }
+        
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("invalid credentials"));
+        
+        return "";
+    }
+    
+    public String logout() {
+        request.getSession().invalidate();
+        
+        return LOGIN_PAGE_REDIRECT;
+    }
+    
+    public String getUsername() {
+        return this.username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
